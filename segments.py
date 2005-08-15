a6a42e360905e0367d4ab22b23531c0383a8ab5a
Kapil Thangavelu <kapilt@gmail.com>
2005-08-15 20:57:42 +0000

    some intermediate work on segments/pipelines

--- segments.py
+++ segments.py
@@ -4,6 +4,27 @@ $Id$
 
 def implements( iface ): pass
 
+class IPipeline( object ):
+    services = " "
+    vars = " "
+    steps = " "
+
+    def process( context ): pass
+
+class IPipeSegment( object ):
+    def process( pipeline, ctxobj ): pass
+
+class IProducer( IPipeSegment ): pass
+class IConsumer( IPipeSegment ): pass
+class IProducerConsumer( IPipeSegment ): pass
+class IFilter( ProducerConsumer ): pass
+class ITee( IPipeSegment ): pass
+class IWatcher( IPipeSegment ): pass
+class IConditionalBranch( IPipeSegment ): pass
+
+
+#################################
+
 class Pipeline( object ):
 
     def __init__(self, **kw):
@@ -19,214 +40,62 @@ class Pipeline( object ):
         for s in self:
             context = s.process( self, context )
 
-
-class IPipeline( object ):
-    services = " "
-    vars = " "
-    steps = " "
-
-    def process( context ): pass
-
-class IPipeSegment( object ):
-
-    def process( pipeline, ctxobj ): pass
-
-class IProducer( PipeSegment ): pass
-class IConsumer( PipeSegment ): pass
-class IProducerConsumer( PipeSegment ): pass
-class IFilter( ProducerConsumer ): pass
-class ITee( PipeSegment ): pass
-class IWatcher( PipeSegment ): pass
-
 class PipeSegment( object ):
 
     def process( self, pipeline, ctxobj ):
         raise NotImplemented
 
+class Producer( PipeSegment ):
 
-#################################
+    def process( self, pipeline, ctxobj ):
+        for res in self.generate():
+            yield res
 
-class PolicyPipe( Pipeline ):
+class Iterator( PipeSegment ):
 
-    def __call__( self, policy ):
-        self.process( policy )
-
-class PipeEnvironmentInitializer( Pipeline ):
-
-    implements( PipeSegment )
-
-    def process( self, pipe, ctxobj ):
-        self.setupServices( pipe, ctxobj )
-        self.setupVariables( pipe, ctxobj )
-        self.setupURIResolver( pipe, ctxobj )
+    def __init__(self, steps ):
+        self.steps = steps
+        
+    def process( self, pipeline, ctxobj ):
+        for obj in ctxobj:
+            for s in self.steps:
+                s.process( pipeline, obj )
         return ctxobj
-    
-    def setupVariables( self, pipe, ctxobj ):
 
-        dfactory = DescriptorFactory( ctxobj )
-        pipe.services["DescriptorFactory"] = DescriptorFactory
-        
+class VariableAggregator( PipeSegment ):
 
-    def setupURIResolver( self, pipe, ctxobj ):
-        resolver = pipe.services['ContentResolver']
-        uri_resolver = resolver.clone( persistent = 0 )
+    def __init__(self, variable_name=""):
+        self.values = []
+        self.variable_name = variable_name
 
-        organization = pipe.services['ContentOrganization']
-        mount_point = organization.getActiveStructure().getCMFMountPoint()
-        mount_path = mount_point.getPhysicalPath()
-        mlen = len('/'.join(mount_path))
-        mount_url_root = mount_point.absolute_url(1)
+    def process(self, pipeline, ctxobj ):
+        pipeline.vars[ self.variable_name ]=self.values
+        self.values.append( ctxobj )
+        return ctxobj
 
-        # should setup an edit method for this
-        uri_resolver.mount_path = mount_url_root
-        uri_resolver.source_host = ctxobj.REQUEST['SERVER_URL']
-        uri_resolver.mlen = len(mount_url_root)
+class VariableIterator( PipeSegment ):
 
-        pipe.services['URIResolver'] = uri_resolver
-        
-        
-    def setupServices( self, pipe, ctxobj ):
-        addService = pipe.services.__setitem__
+    def __init__(self, variable_name, steps ):
+        self.variable_name = variable_name
+        self.steps = steps
 
-        source    = ctxobj.getContentIdentification()
-        addService("ContentIdentification", source)
-        
-        structure = ctxobj.getContentOrganization()
-        addService("ContentOrganization", structure)
-        
-        mastering = ctxobj.getContentMastering()
-        addService("ContentRules", mastering)
-        
-        deployer  = ctxobj.getContentDeployment()
-        addService("ContentTransports", deployer)
-        
-        views     = ctxobj.getContentDirectoryViews()
-        addService("ContentDirectoryViews", views)
-        
-        resolver  = ctxobj.getDeploymentURIs()
-        addService("ContentResolver", resolver )
-        
-        history   = ctxobj.getDeploymentHistory()
-        addService("ContentHistory", history )
-
-        addService("ContentStorage", ContentStorage(self) )
-
-class ContentSource( PipeSegment ):
-
-    implements( IProducer )
-
-    def process( self, pipe, ctxobj ):
-        mlen = pipe.variables.get('mount_length', 0)
-        source = self.getService("ContentIdentification")
-        return source.getContent(mount_length = mlen)
+    def process( self, pipeline, ctxobj ):
+        for value in pipeline.vars.get( self.variable_name, () ):
+            for s in self.steps:
+                s.process( pipeline, value )
 
 
+class ConditionalBranch( PipeSegment ):
 
-class DirectoryViewDeploy( PipeSegment ):
-    """
-    """
+    def __init__(self, branches ):
+        assert isinstance( branches, list)
+        self.branches = branches
 
-    def process( self, pipe, content ):
+    def addBranch( self, condition, step ):
+        self.branches.append( ( condition, step ) )
 
-        views = pipe.services["ContentDirectoryViews"]
-        resolver = pipe.services['URIResolver']
-        store = pipe.services["ContentStorage"]
-        stats = pipe.services['ExecutionStats']
-        
-        contents = views.getContent()
+    def process( self, pipeline, ctxobj ):
+        for condition, step in self.branches:
+            if condition( ctxobj ):
+                return step( pipeline, ctxobj )
 
-        for dvc in contents:
-            resolver.addResource( dvc )
-
-        for dvc in contents:
-            views.cookViewObject( dvc )
-            resolver.resolve( dvc )
-            store( dvc )
-
-class ContentTransport( PipeSegment ):
-
-    def process( self, pipe, ctxobj ):
-        transports = pipe.services['ContentTransports']
-        organization = pipe.services['ContentOrganization']
-        transports.deploy( organization.getActiveStructure() )
-
-
-class ContentPrepPipe( PipeSegment ):
-    """
-    processes a single piece of content
-    """
-    
-    implements( IProducerConsumer )
-
-    def __init__(self):
-        self.prepared = []
-
-    def process( self, pipe, context ):
-
-        # expects an iterator
-        
-        #gc_counter = 0
-        for c in context:
-            self.processContent( c )
-            #if self.gc_threshold%gc_counter == 0:
-
-        return 
-
-    def processContent( self, pipe, content ):
-        factory = pipe.services["DescriptorFactory"]
-        rules = pipe.services["ContentRules"]
-        resolver = pipe.services['URIResolver']
-        
-        content = self.getContent( content )
-        descriptor = factory( content )
-
-        if not rules.match( descriptor ):
-            try: content._p_deactivate()
-            except: pass
-            return
-
-        resolver.addResource( descriptor )
-
-    def getContent( self, ctxobj ):
-        return ctxobj.getObject()
-
-    
-class ContentProcessPipe( PipeSegment ):
-
-    def process( self, pipe, descriptor):
-        rules = pipe.services["ContentRules"]
-        resolver = pipe.services['URIResolver']
-        store = pipe.services["ContentStorage"]
-        
-        rules.render( descriptor )
-        content = descriptor.getContent()
-
-        if not descriptor.isGhost():
-            resolver.resolve( descriptor )
-            
-        store( descriptor )
-
-
-#################################
-# pipeline - todo conditional matching on record or descriptor        
-# segment - uindex / dependency manager /
-
-class DeletionPipeline( object ):
-
-    def process( self, record ):
-        pass
-
-
-class DefaultPolicyPipeline( object ):
-    
-    step_factories = [ PipeEnvironmentInitializer,
-                       ContentSource,
-                       ContentPrepPipe,
-                       DirectoryViewDeploy,
-                       ContentProcessPipe ]
-    
-            
-
-        
-        
-        
