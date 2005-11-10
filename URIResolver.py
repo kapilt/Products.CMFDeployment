70fdb824dda7e480295729df2194a598a98bcf3b
Christian Zagrodnick <zagy@gocept.com>
2005-11-10 15:35:11 +0000

    additionally to getattr we're trying traversal on uri resolve

--- URIResolver.py
+++ URIResolver.py
@@ -150,7 +150,6 @@ class URIResolver:
         """
 
         rnu = None
-        
         if not u:
             return
 
@@ -229,12 +228,16 @@ class URIResolver:
             oid = parts[-1]
         object = getattr( content, oid, None )
         if not object:
+            object = content.unrestrictedTraverse(oid, None)
+        if not object:
             return _marker
         try:
             object_url = object.absolute_url(1)
         except AttributeError:
             res = _marker
         else:
+            if not object_url.startswith('/'):
+                object_url = '/' + object_url
             res = self.uris.get(object_url, _marker)
         
         return res
