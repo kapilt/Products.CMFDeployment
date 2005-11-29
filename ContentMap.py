8503f946e44b7b595d024b0daf4177eb54278c5d
lucie <lucie@sixfeetup.com>
2005-11-29 20:09:45 +0000

    Content Map allows create a tree containing a new descriptor url as a key and his predecesors as a value. It allows to take the reverse dependencies of a descriptor

---
+++ ContentMap.py
@@ -0,0 +1,87 @@
+#class ContentMap
+
+from BTrees.OOBTree import OOBTree
+import pprint
+from URIResolver import normalize
+from Namespace import *
+
+class ContentMap(SimpleItem):
+
+    meta_type = 'Content Map'
+
+    def __init__(self, id=None, resolver=None):
+        self.content_map= OOBTree()
+        self.resolver=resolver
+        self.id= id
+        
+    def addContentMap(self, url_key, physical_path):
+        #add a new descriptor url as a key
+        #and his referencer as a value
+        result = self.content_map.get(url_key)
+        if(result):
+            #look if it is already in the list
+            if not (physical_path in self.content_map[url_key]):
+                self.content_map[url_key].append(physical_path)
+        else:
+            self.content_map[url_key]= [physical_path]
+        
+        
+    def getKeyURL(self, descriptor):
+        relative_url = descriptor.getSourcePath() or descriptor.content_url
+        #print "\n***** relative_url: ", relative_url
+        content_path = descriptor.getContentPath()
+        
+        if content_path is None:
+            content_path=''
+            mlen = len(self.resolver.mount_path)
+            #print "mlen: ", mlen
+                
+            #print "coupage 1: ", relative_url.rfind('/')             
+            url_context  = relative_url[:relative_url.rfind('/')]
+            #print "url_context: ", url_context
+            cut2= url_context.rfind('/')
+            #print "coupage 2: ", cut2 
+            if (cut2 != -1):
+                content_path = url_context[url_context.rfind('/'):]
+            #print "-> content_path: ", content_path       
+            
+            #print "filename: ", descriptor.getFileName()
+            #print "target: ", self.resolver.target_path
+
+        nu = normalize('/'.join( (self.resolver.target_path,
+                                            content_path,
+                                            descriptor.getFileName() ) ),
+                                 '/'
+                                 )
+        #print "nu: ", nu
+        return nu
+        
+        
+    def getReverseDependencies(self, descriptor): #url):  
+        #get the referencers of a descriptor       
+        nu= self.getKeyURL(descriptor)
+        result= self.content_map.get(nu)
+        if (not result):
+            result= []
+        #print "####### ContentMap: getReverseDependencies: result: ", result
+        return result
+        
+        
+    def pprint(self):
+        pprint.pprint(dict(self.content_map.items()))
+        
+        
+    def clean(self, descriptor):
+        #clean the content map when an object has been deleted
+        #remove all the urls in the contentmap 
+        #print "####### ContentMap: clean"
+        key_url = self.getKeyURL(descriptor)
+        if key_url in self.content_map.items():
+            del self.content_map[key_url]
+        
+        value_url= "/"+descriptor.getContentURL()
+        for k,v in self.content_map.items():
+            if value_url in v:
+                self.content_map[k].remove(value_url)       
+        
+        
