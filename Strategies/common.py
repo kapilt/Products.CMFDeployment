5ea7049913c29786702a3e71a142060e9b35c657
Kapil Thangavelu <kapilt@gmail.com>
2006-02-06 20:52:09 +0000

    remove strategies implementation, fix for deploy user ui (from previous trunk)

--- Strategies/common.py
+++
@@ -1,6 +0,0 @@
-from Products.CMFDeployment.Log import LogFactory
-from Products.CMFDeployment.ContentStorage import ContentStorage
-from Products.CMFDeployment.Statistics import TimeStatistics, MemoryStatistics
-from Products.CMFDeployment.Descriptor import ContentDescriptor, DescriptorFactory
-
-log = LogFactory('Strategy')
